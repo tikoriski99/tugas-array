d21c8e76b43c01071feea7e964bdfbd1608fe7dc
tikoriski99 <56874072+tikoriski99@users.noreply.github.com>
2019-11-14 11:35:42 +0700

    Add files via upload

---
+++ java array.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikoriskiarray;
+
+/**
+ *
+ * @author User
+ */
+public class Tikoriskiarray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String [] orangtua = {"hadi santoso","sulikah"};
+    String [] anak_kandung = {"noviatin","tiko riski darmawan","wirnawati"};
+    String [] kakek = {"kasenan"};
+    String [] nenek = {"kasanah"};
+    String [] teman = {"hikam","darul","fery","toni","ridho"};
+    
+    System.out.println(orangtua[0]);
+    System.out.println(orangtua[1]);
+    System.out.println (anak_kandung[0]);
+    System.out.println (anak_kandung[1]);
+    
+    String [] saya_anakke_dua = {"nama saya:"};
+    String [] saya = {"TIKO RISKI DARMAWAN"};
+    System.out.println(saya_anakke_dua[0]);
+    System.out.println(saya[0]);
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
